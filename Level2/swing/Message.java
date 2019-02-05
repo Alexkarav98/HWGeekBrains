e6aa01017f11be07c405cd4f1711a9b33941c2a2
Alexkarav32 <djmyaso@mail.ru>
2019-02-05 10:36:20 +0300

    HW4Swing

---
+++ Level2/swing/Message.java
@@ -0,0 +1,20 @@
+package swing;
+
+public class Message {
+    private String userName;
+
+    private String message;
+
+    public Message(String userName, String message) {
+        this.userName = userName;
+        this.message = message;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
